e0b10f13b285719926a9f298ad8e20e084f0f8df
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-09-24 12:22:59 +0900

    🎉 Reactプロジェクト作成

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>テキスト変更</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
