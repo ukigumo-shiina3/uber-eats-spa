2a22056d052cad9d0b580c72d322527235499314
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-10-04 21:57:14 +0900

    ✨  仮注文詳細のAPIを叩く

--- frontend/src/containers/Orders.jsx
+++ frontend/src/containers/Orders.jsx
@@ -1,5 +1,28 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useReducer } from "react";
+
+import { fetchLineFoods } from "../apis/line_foods";
+
+import {
+  initialState,
+  lineFoodsActionTyps,
+  lineFoodsReducer,
+} from "../reducers/lineFoods";
 
 export const Orders = () => {
+  const [state, dispatch] = useReducer(lineFoodsReducer, initialState);
+  useEffect(() => {
+    dispatch({ type: lineFoodsActionTyps.FETCHING });
+    fetchLineFoods()
+      .then((data) =>
+        dispatch({
+          type: lineFoodsActionTyps.FETCH_SUCCESS,
+          payload: {
+            lineFoodsSummary: data,
+          },
+        })
+      )
+      .catch((e) => console.error(e));
+  }, []);
+
   return <Fragment>注文画面</Fragment>;
 };
